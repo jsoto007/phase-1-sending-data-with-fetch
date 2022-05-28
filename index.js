6b9e05190003b573e6bf86649769d5d13c526b31
Jose Soto <jsoto7087@gmail.com>
2022-05-28 14:32:38 -0400

    commit tree 956fd855c875caed90147983288af037bc239e2c

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
+// https://github.com/jsoto007/phase-1-sending-data-with-fetch
+
 // Add your code here
+// const submitData = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         dogName: "Bryron",
+//         dogBreed: " Poodle",
+//     }), // data goes here
+// };
+
+
+function submitData(name, email) {
+    const postConfig = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        }),
+    };
+
+    return fetch('http://localhost:3000/users', postConfig)
+        .then(function (res) {
+            console.log(res);
+            return res.json();
+        })
+        .then(function (object) {
+            console.log(object);
+        })
+        .catch(function (error) {
+            alert("something went wrong: check your code!")
+        })
+
+}
+
+
+// <
+// input class = "vector-search-box-input"
+// type = "search"
+// name = "search"
+// placeholder = "Search Wikipedia"
+// aria - label = "Search Wikipedia"
+// autocapitalize = "sentences"
+// title = "Search Wikipedia [ctrl-option-f]"
+// accesskey = "f"
+// id = "searchInput" > < /input>
